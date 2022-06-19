70e2d5c02edfb83b165c47714fc18701918cc465
Azizul kabir lavlu <lavlu.bcse@gmail.com>
2022-06-19 17:09:35 +0600

    footer

--- src/pages/Shared/Footer/Footer.js
+++ src/pages/Shared/Footer/Footer.js
@@ -8,20 +8,20 @@ const Footer = () => {
         <div className='footer'>
             <div className='footer-section'>
                 <div className='footer-item'>
-                    <h1>Alzoya Books Management</h1>
-                    <ul>Help</ul>
+                    <h1 className='text-xl'>Alzoya Books Management</h1>
+                    <ul>Help </ul>
                     <ul>Order Status</ul>
                     <ul>Delivery Choice</ul>
                     
                 </div>
                 <div className='footer-item'>
-                    <h1>Contact us</h1>
+                    <h3>Contact us</h3>
                     <ul>47/5, Nikunjo-1, Dhaka-1229</ul>
                     <ul>01754862546</ul>
                     <ul>info@adana.com</ul>
                 </div>
                 <div className='footer-item'>
-                    <h1>Working hours</h1>
+                    <h3>Working hours</h3>
                     <ul>Sat-Thu: 9.00am-6.00pm</ul>
                     <ul>Tuesday: 9.00am-6.00pm</ul>
                     <ul>Friday: Holiday</ul>
